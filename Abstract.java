4b4c0613df4201a9ce41da66570f29d26ac1833f
VIRAJ PARMAR <116947575+VirajParmar15@users.noreply.github.com>
2024-01-12 10:25:01 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,13 @@
+class base2{
+
+    public base2(){
+        System.out.println("mai base2 ka constructor hu");
+    }
+
+    public void sayHello(){
+        System.out.println("hello");
+    }
+
+}
+public class Abstract {
+}
