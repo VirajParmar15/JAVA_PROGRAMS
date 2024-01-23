0aa945e890985b38d87cfafd2bd77bd02b620806
VIRAJ PARMAR <116947575+VirajParmar15@users.noreply.github.com>
2024-01-23 20:24:55 +0530

    Add files via upload

---
+++ ch1_1.java
@@ -0,0 +1,16 @@
+public class ch1_1 {
+    public static void main(String[] args) {
+        sayname();
+        sayage();
+    }
+
+    public static void sayname() {
+        System.out.println("viraj");
+
+    }
+
+    public static void sayage() {
+        System.out.println("18");
+
+    }
+}
---
+++ check.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class check {
+    public static void main(String[] args) {
+
+        //Scanner c1 = new Scanner(System.in);
+        //System.out.println("enter a number");
+        //System.out.println(c1.hasNextInt());
+        //check integer or not integer via boolean
+
+        //int a = 3;
+        //a--;
+        //System.out.println(a);
+
+        //int y = 7;
+        //int x = (++y) * 8;
+        //System.out.println(x);
+    }
+}
+//next for practice
+
+//  1...(x-y)/2
+//  2...(b*b-4ac)/2a
+//  3...(v*v)-(u*u)
+//  4...a*b-d
